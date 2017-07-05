6415491603d35fdc933729148dde1e1df6c63fb0
Adam Ayd <adamayd@icloud.com>
2017-07-04 23:41:08 -0400

    commented out debugging lines

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 var date = new Date();
-console.log(date);
+// console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+// console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
 let secPosition = sec * 6;
 let minPosition = (min * 6) + (secPosition / 60);
